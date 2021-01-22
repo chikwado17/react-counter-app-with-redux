2c2ef143310aab392ea01f5f57a6dfe2aec688d0
chikwado17 <chikwadonworie@gmail.com>
2021-01-22 10:47:17 +0100

    initial project setup

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <p>App Component</p>
+            </div>
+         );
+    }
+}
+ 
+export default App;
