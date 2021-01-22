ca1396716d4b7d186c5874f28093eea707c892dd
chikwado17 <chikwadonworie@gmail.com>
2021-01-22 12:05:07 +0100

    completed counter app project with reactjs and redux

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { incrementCount, resetCount, decrementCount } from '../redux/counter/counterAction';
 
+const mapStateToProps = (state) => ({
+    counter: state.counter.count
+});
 
+const mapDispatchToProps = {
+    incrementCount,
+    resetCount,
+    decrementCount
+}
 
 class Counter extends React.Component {
-    state = {  }
+
+
+    handleIncrementCouunt = () => {
+        this.props.incrementCount()
+    };
+
+    handleResetCount = () => {
+        this.props.resetCount()
+    };
+
+    handleDecrementCount = () => {
+        this.props.decrementCount()
+    };
+
+
     render() { 
+        const { counter } = this.props;
         return ( 
             <div className="ui segment">
-                <h1>Counter - {0}</h1>
+                <h1>Counter : {counter}</h1>
 
-                <button className="ui primary button">Increment</button>
-                <button className="ui red button">Reset</button>
-                <button className="ui primary button">Decrement</button>
+                <button onClick={this.handleIncrementCouunt} className="ui primary button">Increment</button>
+                <button onClick={this.handleResetCount} className="ui red button">Reset</button>
+                <button onClick={this.handleDecrementCount} className="ui primary button">Decrement</button>
             </div>
          );
     }
 }
  
-export default Counter;
+export default connect(mapStateToProps,mapDispatchToProps)(Counter);
